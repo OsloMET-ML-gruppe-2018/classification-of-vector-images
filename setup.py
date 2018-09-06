4d83de65ff5be3f1f0aaffd8157b077f827e9cb8
Sondre <sondre.hal@gmail.com>
2018-09-06 09:49:44 +0000

    created project structure and added README

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data-science and Machine learning project at OsloMET',
+    author='OsloMET',
+    license='MIT',
+)
