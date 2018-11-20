86797f34a209eb5f819dba2f46f3809c96fa3ae9
sondre <sondre.hal@gmail.com>
2018-11-20 15:26:23 +0100

    Refactor data analytics scripts

--- src/data/classifications.py
+++ src/data/classifications.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 import math
 import csv
-
+from pprint import pprint as pp
+import json
 
 def classification_overlap(class_label:str):
     path_str = "../../data/raw/categories/categories/{}.txt".format(class_label)
@@ -13,22 +14,22 @@ def classification_overlap(class_label:str):
         read_data = f.read()
         class_label_list = string_to_list(read_data)
 
+    result_data_dict = {"label": class_label, "label_image_count": len(class_label_list), "result": []}
     lablel_dict = gen_dict_of_label_lists(class_label)
-    perc_dict = dict.fromkeys(lablel_dict.keys(), 0)
     unique_ids = set(class_label_list)
+    class_label_id_in_other_lists = set()
 
     for label_list in lablel_dict:
         unique_ids = unique_ids.union(lablel_dict.get(label_list))
         count = 0
         for id in class_label_list:
             if id in lablel_dict.get(label_list):
+                class_label_id_in_other_lists.add(id)
                 count += 1
-        perc_dict[label_list] = (count / len(lablel_dict.get(label_list))) * 100
-    print("For label: " + class_label + " - size: " + str(len(class_label_list)))
-    for label in perc_dict:
-        print(str(label) + " % overlap : " + str(perc_dict.get(str(label))) + "<br>")
-    print("\nSum of unique images in categories: " + str(len(unique_ids)))
-
+        result_data_dict["result"].append({"label": label_list, "image_count": len(lablel_dict.get(label_list)),
+                                           "overlap": count, "overlap_percent": (count / len(class_label_list)) * 100})
+        result_data_dict["total_image_overlap"] = len(class_label_id_in_other_lists)
+    return result_data_dict
 
 
 def gen_dict_of_label_lists(not_include_label:str) -> dict:
@@ -48,8 +49,10 @@ def gen_dict_of_label_lists(not_include_label:str) -> dict:
 def string_to_list(string:str) -> iter:
     return set(string.split("\n"))
 
+
 def remove_file_ending(filename:str):
     return filename.split(".")[0]
+
 
 def write_to_csv(class_label_list, label_dict, perc_dict):
     file_path = Path("../../data/processed/overlap.csv")
@@ -58,13 +61,22 @@ def write_to_csv(class_label_list, label_dict, perc_dict):
                                            "image_overlap_amount", "overlap_percent"])
 
 
-
-
 def main():
     labels = gen_dict_of_label_lists("")
+
+    result = []
+    count = 1
     for el in labels:
         print("\n")
-        classification_overlap(el)
+        result.append(classification_overlap(el))
+        count += 1
+    pp(result)
+    json_object = json.dumps(result)
+    json_file_path = Path("../../data/processed/dataset_analytics/dataset_report.json")
+
+    with json_file_path.open("w+") as writer:
+        writer.write(json_object)
+
 
 if __name__ == '__main__':
     main()
---
+++ src/data/create_data_analytics.py
@@ -0,0 +1,100 @@
+from pathlib import Path
+from pprint import pprint as pp
+import csv
+import json
+
+def get_mtruk_categories():
+    path = Path("/home/sondre/PycharmProjects/classification-of-vector-images/data/raw/categories/cats_mturk")
+    categories ={}
+    for label_file in path.iterdir():
+        with label_file.open("r") as reader:
+            images = ""
+            lines = reader.readlines()
+            for line in lines:
+                if line[0] != "#":
+                    images += line
+            categories[label_file.name.split(".")[0]] = images.split()
+    return categories
+
+def get_public_categories():
+    path = Path("../../data/raw/categories/categories/")
+    categories ={}
+    for label_file in path.iterdir():
+        with label_file.open("r") as reader:
+            images = ""
+            lines = reader.readlines()
+            for line in lines:
+                if line[0] != "#":
+                    images += line
+            categories[label_file.name.split(".")[0]] = images.split()
+    return categories
+
+def get_category_sizes(cat_dict):
+    result = []
+    for label, images in cat_dict.items():
+        result.append({"category": label, "image_count": len(images)})
+    return result
+
+
+def calc_category_overlap(category_image_dict):
+    category_image_dict = setify(category_image_dict)
+
+    result_list = []
+    for label, images in category_image_dict.items():
+        images_in_other_categories = set()
+        result_data_dict = {"label": label, "label_image_count": len(images), "result": []}
+        for comp_label, comp_images in category_image_dict.items():
+            if label == comp_label:
+                continue
+
+            overlap_count = 0
+
+            for image in images:
+                if image in comp_images:
+                    overlap_count += 1
+                    images_in_other_categories.add(image)
+            result_data_dict["result"].append({"label": comp_label, "image_count": len(comp_images),
+                                           "overlap": overlap_count,
+                                           "overlap_percent": (overlap_count / len(images)) * 100})
+        result_data_dict["total_image_overlap"] = len(images_in_other_categories)
+        result_list.append(result_data_dict)
+
+    return result_list
+
+
+def setify(category_image_dict):
+    for label, images in category_image_dict.items():
+        category_image_dict[label] = set(images)
+    return category_image_dict
+
+
+def get_category_sizes():
+    mturk_cat = get_mtruk_categories()
+    public_cat = get_public_categories()
+
+    mturk_images = set()
+    for cat, images in mturk_cat.items():
+        for image in images:
+            mturk_images.add(image)
+
+    public_images = set()
+    for cat, images in public_cat.items():
+        for image in images:
+            public_images.add(image)
+
+    print("mkat count: " + str(len(mturk_images)))
+    print("public count: " + str(len(public_images)))
+
+
+def create_mturk_dataset():
+    store_file_path = Path("../../data//processed/dataset_analytics/mturk_data.json")
+    result =get_mtruk_categories()
+    sizes = get_category_sizes(result)
+    data = calc_category_overlap(result)
+    data = json.dumps(data)
+    with store_file_path.open("w+") as writer:
+        writer.write(data)
+
+
+if __name__ == "__main__":
+    get_category_sizes()
